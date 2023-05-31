a1c967861ba83e0e381cd1fc69e00dafff63f25a
Jorge <jorgecolmenarez89@gmail.com>
2023-05-31 11:09:59 -0400

    subir potho de perfil

--- screens/cliente/ProfileScreen.js
+++ screens/cliente/ProfileScreen.js
@@ -1,7 +1,8 @@
-import React, {useState, useEffect, useContext} from 'react';
-import {View, Text, StyleSheet, TextInput, Alert, Platform} from 'react-native';
+import React, {useState, useContext} from 'react';
+import {View, Text, StyleSheet, TextInput, Alert, Platform, PermissionsAndroid} from 'react-native';
 import storage from '@react-native-firebase/storage';
-import {Avatar, Button, Image} from '@rneui/themed';
+import {useFocusEffect} from '@react-navigation/native';
+import {Avatar, Button, BottomSheet, Icon} from '@rneui/themed';
 import * as ImagePicker from 'react-native-image-picker';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import SelectDropdown from 'react-native-select-dropdown';
@@ -22,18 +23,22 @@ function ProfileScreen({navigation}) {
   const [defaultSex, setDefaultSex] = useState('');
   const [url, setUrl] = useState(null);
 
-  useEffect(() => {
-    if (userLoged.fullName) setFullName(userLoged.fullName);
-    if (userLoged.phoneNumber) setPhone(userLoged.phoneNumber);
-    if (userLoged.sexo) {
-      setDefaultSex(userLoged.sexo);
-      setSex(userLoged.sexo);
-    }
-    if (userLoged.url && userLoged.url != '' && userLoged.url != null) {
-      setFileData({assets: [{uri: userLoged.url}]});
-      setUrl(userLoged.url);
-    }
-  }, []);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      if (userLoged.fullName) setFullName(userLoged.fullName);
+      if (userLoged.phoneNumber) setPhone(userLoged.phoneNumber);
+      if (userLoged.sexo) {
+        setDefaultSex(userLoged.sexo);
+        setSex(userLoged.sexo);
+      }
+      if (userLoged.url && userLoged.url != '' && userLoged.url != null) {
+        setFileData({assets: [{uri: userLoged.url}]});
+        setUrl(userLoged.url);
+      }
+    }, []),
+  );
 
   const loadFile = () => {
     let options = {
@@ -45,7 +50,6 @@ function ProfileScreen({navigation}) {
       },
     };
     ImagePicker.launchImageLibrary(options, response => {
-      //console.log('Response = ', response);
       if (response.didCancel) {
         console.log('User cancelled image picker');
       } else if (response.error) {
@@ -55,10 +59,6 @@ function ProfileScreen({navigation}) {
         alert(response.customButton);
       } else {
         const source = {uri: response.uri};
-        // You can also display the image using data:
-        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
-        // alert(JSON.stringify(response));s
-        /*console.log('response', JSON.stringify(response));*/
         setFileData(response);
         response.assets.forEach(item => {
           console.log('item', item.uri);
@@ -72,6 +72,62 @@ function ProfileScreen({navigation}) {
         });
       }
     });
+  };
+
+  const openCamera = async () => {
+    const grantedcamera = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA, {
+      title: 'App Camera Permission',
+      message: 'App needs access to your camera ',
+      buttonNeutral: 'Ask Me Later',
+      buttonNegative: 'Cancel',
+      buttonPositive: 'OK',
+    });
+    const grantedstorage = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+      {
+        title: 'App Camera Permission',
+        message: 'App needs access to your camera ',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (
+      grantedcamera === PermissionsAndroid.RESULTS.GRANTED &&
+      grantedstorage === PermissionsAndroid.RESULTS.GRANTED
+    ) {
+      console.log('Camera & storage permission given');
+      const options = {
+        mediaType: 'photo', //to allow only photo to select ...no video
+        saveToPhotos: true, //to store captured photo via camera to photos or else it will be stored in temp folders and will get deleted on temp clear
+        includeBase64: false,
+      };
+      ImagePicker.launchCamera(options, res => {
+        if (res.didCancel) {
+          console.log('User cancelled image picker');
+        } else if (res.error) {
+          console.log('ImagePicker Error: ', res.error);
+        } else if (res.customButton) {
+          console.log('User tapped custom button: ', res.customButton);
+          alert(res.customButton);
+        } else {
+          const source = {uri: res.uri};
+          setFileData(res);
+          res.assets.forEach(item => {
+            console.log('item', item.uri);
+            const {uri} = item;
+            const filename = 'avatar';
+            const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri;
+            const path = `images/${userLoged.id}/${filename}`;
+            uploadImageAndGetUrl(uploadUri, path).then(url => {
+              setUrl(url);
+            });
+          });
+        }
+      });
+    } else {
+      console.log('Camera permission denied');
+    }
   };
 
   const handleUpdate = async () => {
@@ -134,7 +190,7 @@ function ProfileScreen({navigation}) {
               <Avatar.Accessory
                 size={23}
                 style={{backgroundColor: '#0b445e'}}
-                onPress={() => loadFile()}
+                onPress={() => setIsVisible(true)}
               />
             </Avatar>
           )}
@@ -151,7 +207,7 @@ function ProfileScreen({navigation}) {
                   <Avatar.Accessory
                     size={23}
                     style={{backgroundColor: '#66bfc5'}}
-                    onPress={() => loadFile()}
+                    onPress={() => setIsVisible(true)}
                   />
                 </Avatar>
               </View>
@@ -234,6 +290,58 @@ function ProfileScreen({navigation}) {
           />
         </View>
       </View>
+
+      <BottomSheet modalProps={{}} isVisible={isVisible}>
+        <View style={{backgroundColor: 'white', paddingHorizontal: 20, paddingVertical: 20}}>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}>
+            <Text style={styles.titleShet}>Foto de Perfil</Text>
+            <View>
+              <Icon
+                name={'close'}
+                color={'#06060a'}
+                size={27}
+                type='ionicon'
+                style={{marginTop: 5, marginLeft: 5}}
+                onPress={() => setIsVisible(false)}
+              />
+            </View>
+          </View>
+
+          <View style={{display: 'flex', flexDirection: 'row', marginTop: 20}}>
+            <View style={styles.itemShet}>
+              <View style={styles.itemShetRound}>
+                <Icon
+                  name={'camera-outline'}
+                  color={'#0b445e'}
+                  size={27}
+                  type='ionicon'
+                  onPress={() => openCamera()}
+                />
+              </View>
+              <Text style={styles.subTitleShet}>Cámara</Text>
+            </View>
+
+            <View style={styles.itemShet}>
+              <View style={styles.itemShetRound}>
+                <Icon
+                  name={'image-outline'}
+                  color={'#0b445e'}
+                  size={27}
+                  type='ionicon'
+                  onPress={() => loadFile()}
+                />
+              </View>
+              <Text style={styles.subTitleShet}>Galería</Text>
+            </View>
+          </View>
+        </View>
+      </BottomSheet>
     </View>
   );
 }
@@ -295,5 +403,31 @@ const styles = StyleSheet.create({
   imageBox: {
     width: 300,
     height: 300,
+  },
+  itemShet: {
+    display: 'flex',
+    width: 70,
+    marginRight: 20,
+    alignItems: 'center',
+  },
+  itemShetRound: {
+    borderRadius: 50,
+    borderColor: '#7d7d7d',
+    borderWidth: 1,
+    height: 60,
+    width: 60,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleShet: {
+    fontSize: 15,
+    color: '#06060a',
+    fontFamily: 'Poppins-SemiBold',
+  },
+  subTitleShet: {
+    fontSize: 14,
+    color: '#06060a',
+    fontFamily: 'Poppins-Regular',
   },
 });
